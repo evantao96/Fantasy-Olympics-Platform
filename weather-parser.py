f571d36b1ea354cc8ac0216a39f4d5f4a77be1fe
Evan Tao <evantao@Evans-MacBook-Pro-4.local>
2024-02-01 18:26:53 -0500

    refactored weather parser code

--- weather-parser.py
+++ weather-parser.py
@@ -1,50 +1,48 @@
-import requests
 from bs4 import BeautifulSoup
 import mysql.connector
 from mysql.connector import Error
+import requests
 import ast
 
-class WeatherParser():
+locations = [("Athens", "Greece"), ("London", "UK"), 
+            ("Paris", "France"), ("Istanbul", "Turkey"),
+            ("Stockholm", "Sweden"), ("Amsterdam", "Netherlands"),
+            ("Berlin", "Germany"), ("Helsinki", "Finland"),
+            ("Melbourne", "Australia"), ("Rome", "Italy"),
+            ("Seoul", "South-Korea"), ("Barcelona", "Spain"),
+            ("Beijing", "China")]
+years = (2012, 2013)
+months = (1, 3)
 
-    def __init__(self, locations_dates):
-        self.locations_dates = locations_dates
+def generate_settings(locations, start_year, end_year, start_month, end_month): 
+    settings = []
+    for l in locations:
+        for y in range(start_year, end_year):
+            for m in range(start_month, end_month):
+                settings.append((l, (m, y)))
+    return settings
 
-    def parse(self):
-        result = dict()
+def get_html_page(city, country, year, month):
+    page = requests.get("https://www.timeanddate.com/weather/{}/{}/historic?month={}&year={}".format(country, city, month, year))
+    return page.content
 
-        for location, date in self.locations_dates:
-            result[(location, date)] = self.__get_data(location, date)
+def get_temperature_and_humidity(city, country, year, month): 
+    html_doc = get_html_page(city, country, year, month)
+    soup = BeautifulSoup(html_doc, "html.parser")
+    block = soup.find("th", text="Average")
+    if block is not None:
+        value = block.parent
+        if value is not None:
+            temperature = value.find("td").text[:-3]
+            humidity = value.find("td", {"class": "sep"}).text[:-1]
+            return (temperature, humidity)
+        else:
+            print("Page has no HTML parent")
+    else:
+        print("Page has no temperature and humidity section")
+    return {}
 
-        return result
-
-    def __get_html(self, location, date):
-        city, country = location
-        month, year = date
-        page = requests.get("https://www.timeanddate.com/weather/{}/{}/historic?month={}&year={}".format(country, city, month, year))
-        return page.content
-
-    def __get_data(self, location, date):
-        html_doc = self.__get_html(location, date)
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        span = soup.find("th", text="Average")
-
-        if span is not None:
-            value = span.parent
-        
-            if value is not None:
-                temperature = value.find("td").text[:-3]
-                humidity = value.find("td", {"class": "sep"}).text[:-1]
-
-                data = dict()
-
-                data["temperature"] = temperature
-                data["humidity"] = humidity
-
-                return data
-
-        return None
-
-def insert(sql):
+def insert(connection, sql):
     """
     Executes SQL command, provided as input as a string
     """
@@ -56,40 +54,18 @@ def insert(sql):
         print("Error executing {}".format(sql))
 
 if __name__ == "__main__":
-
     try: 
         connection = mysql.connector.connect(host='database-1.c32yscgymlt6.us-east-1.rds.amazonaws.com',
             database='db1',user='evantao',password='rubyonrails')
             
         if connection.is_connected():
-            db_Info = connection.get_server_info()
-            print("Connected to MySQL Server version", db_Info)
+            print("Connected to MySQL database")
             cursor = connection.cursor()
     except Error as e:
         print("Error while connecting to MySQL", e)
 
-    locations = [("Athens", "Greece"), ("London", "UK"), 
-                ("Paris", "France"), ("Istanbul", "Turkey"),
-                ("Stockholm", "Sweden"), ("Amsterdam", "Netherlands"),
-                ("Berlin", "Germany"), ("Helsinki", "Finalnd"),
-                ("Melbourne", "Australia"), ("Rome", "Italy"),
-                ("Seoul", "South-Korea"), ("Barcelona", "Spain"),
-                ("Beijing", "China")]
-
-    locations_dates = []
-    for location in locations:
-        for year in range(2014, 2016):
-            for month in [1, 6]:
-                date = (month, year)
-                locations_dates.append((location, date))
-
-    w = WeatherParser(locations_dates)
-    data = w.parse()
-
-    for key, val in data.items():
-        if val is not None and "humidity" in val:
-            location, date = key
-            city, country = location
-            month, year = date
-            insert("INSERT INTO Weather (City, Country, Month, Year, Temp, Humidity) VALUES ('{}', '{}', {}, {}, {}, {})".format(city, country, month, year, val["temperature"], val["humidity"]))
+    locations_dates = generate_settings(locations, years[0], years[1], months[0], months[1])
+    for (city, country), (year, month) in locations_dates:
+        temperature, humidity = get_temperature_and_humidity(city, country, year, month)
+        insert("INSERT INTO Weather (City, Country, Month, Year, Temp, Humidity) VALUES ('{}', '{}', {}, {}, {}, {})".format(city, country, month, year, val["temperature"], val["humidity"]))
     connection.close()
